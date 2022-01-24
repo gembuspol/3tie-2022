8d03c70ec5d625afa2729274267d27c66caf8c25
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-01-24 15:20:02 +0100

    lekcja3
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -2,13 +2,15 @@ import pygame
 
 def main():
     pygame.init()
-    pygame.display.set_mode((300,300),0,32)
+    Oknogry=pygame.display.set_mode((300,300),0,32)
     run=True
     while(run):
         pygame.time.delay(100)
-        print("Program działa")
+        kwadrat = pygame.Rect((150,150),(20,20))
+        pygame.draw.rect(Oknogry,(255,0,0),kwadrat)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        pygame.display.update()
 
 main()
