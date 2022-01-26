2c60651e0dd653cf0a91f1dd773c041be48609fc
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-01-26 13:05:01 +0100

    lekcja4
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -4,13 +4,32 @@ def main():
     pygame.init()
     Oknogry=pygame.display.set_mode((300,300),0,32)
     run=True
+    zmienna1=150
+    zmienna2=150
     while(run):
+        Oknogry.fill((0,0,0))
         pygame.time.delay(100)
-        kwadrat = pygame.Rect((150,150),(20,20))
+        kwadrat = pygame.Rect((zmienna1,zmienna2),(20,20))
         pygame.draw.rect(Oknogry,(255,0,0),kwadrat)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            #sterowanie weża
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    zmienna1=zmienna1-20
+                elif event.key == pygame.K_RIGHT:
+                    zmienna1=zmienna1+20 
+                elif event.key == pygame.K_UP:
+                    zmienna2=zmienna2-20
+                elif event.key == pygame.K_DOWN:
+                    zmienna2=zmienna2+20   
+        #zmienna1=zmienna1+20
+        if zmienna1>300:
+            zmienna1=0
+        #zmienna2=zmienna2+40
+        if zmienna2>300:
+            zmienna2=0
         pygame.display.update()
 
 main()
