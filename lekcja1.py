7741dfcd8a6343f1fd5098ad5b48414aca2565df
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-02-02 13:02:26 +0100

    lekcja6
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- lekcja1.py
+++ lekcja1.py
@@ -1,16 +1,23 @@
 import pygame
+import random
+
+#losowanie pozycji jablka
+xApple=random.randint(0,14)*20+10
+yApple=random.randint(0,14)*20+10
 
 def main():
     pygame.init()
     Oknogry=pygame.display.set_mode((300,300),0,32)
     run=True
-    zmienna1=150
-    zmienna2=150
+    zmienna1=160
+    zmienna2=160
     while(run):
         Oknogry.fill((0,0,0))
         pygame.time.delay(100)
         kwadrat = pygame.Rect((zmienna1,zmienna2),(20,20))
         pygame.draw.rect(Oknogry,(255,0,0),kwadrat)
+        #rysowanie jablka
+        pygame.draw.circle(Oknogry,(0,255,0),(xApple,yApple),10)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -24,12 +31,18 @@ def main():
                     zmienna2=zmienna2-20
                 elif event.key == pygame.K_DOWN:
                     zmienna2=zmienna2+20   
-        #zmienna1=zmienna1+20
+        #przejscie prawa
         if zmienna1>300:
             zmienna1=0
-        #zmienna2=zmienna2+40
+        #przejscie dol
         if zmienna2>300:
             zmienna2=0
+        #przejscie lewa
+        if zmienna1<0:
+            zmienna1=300
+        #przejscie gora
+        if zmienna2<0:
+            zmienna2=300
         pygame.display.update()
 
 main()
