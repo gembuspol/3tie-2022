c22b8684c4caf0570708a706470d183fc435b7fd
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-05-13 15:07:40 +0200

    losowanie liczby
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- znajdzLiczbe/skrypt.js
+++ znajdzLiczbe/skrypt.js
@@ -1,4 +1,17 @@
+var wylosowanaLiczba=Math.floor(Math.random()*100)+1;
 function losuj(){
     var ilosc=document.getElementById('zakres').value;
     document.getElementById('wybranaWartosc').value=ilosc;
+    wylosowanaLiczba=Math.floor(Math.random()*ilosc)+1;
+}
+
+function sprawdz(){
+    let dane=document.getElementById("liczbaUzytkownika")
+    if(dane.value == wylosowanaLiczba){
+        alert("Wygrałeś");
+    }else if(dane.value<wylosowanaLiczba){
+        alert("Za mała liczba");
+    }else{
+        alert("Za duża liczba");
+    }
 }
